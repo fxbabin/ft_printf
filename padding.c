7339eeab44e1d8a2b0f8a006498503b49704d877
Francois-xavier BABIN <fbabin@e3r11p8.42.fr>
2018-01-03 13:31:55 +0100

    42

--- padding.c
+++ padding.c
@@ -6,7 +6,7 @@
 /*   By: misteir <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/25 18:32:33 by misteir           #+#    #+#             */
-/*   Updated: 2018/01/03 00:03:00 by fbabin           ###   ########.fr       */
+/*   Updated: 2018/01/03 13:30:51 by fbabin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,68 +44,40 @@ void	addzeros(t_printf*t, char *buff, int nb, int start)
 	int		i;
 
 	i = -1;
-	//if (arg[0] == '-')
-	//	ft_memcpy(buff + ++i, "-", 1);
-	if (t->prec > 0 && t->prec < t->nb && t->minus == 0)
+	if (t->prec > 0 && t->nb > t->prec && t->minus == 0 && !t->neg)
 	{
-		
 		addspaces(t, buff, t->nb - t->prec, 0);
 		start += t->nb - t->prec;
 		start -= (t->plus && !t->neg) ? 1 : 0;
-		//start -= (!t->plus && t->neg) ? 1 : 0;
 		nb += (t->plus) ? 1 : 0;
-		//nb++;
 	}
-	//start -= (t->plus && !t->neg) ? 1 : 0;
-	//nb -= (t->plus && t->nb < t->prec) ? 0 : 1;
 	nb += (t->plus && t->minus) ? 1 : 0;
 	if (t->plus && !t->neg && nb--)
 		ft_memcpy(buff + start + ++i, "+", 1);
 	if (t->neg && nb--)
 		ft_memcpy(buff + start + ++i, "-", 1);
+	nb += ((t->nb - t->prec) == 1 && t->neg) ? 1 : 0;
+	nb += (t->prec > t->nb && t->neg) ? 1 : 0;
 	while (nb-- > 0)
 		ft_memcpy(buff + start + ++i, "0", 1);
 }
-
-/*void		s(char *str, const char *arg, int i1, int i2)
-  {
-  str[i1] = '-';
-  str[i2] = '0';
-  (void)arg;
-  }*/
 
 void		copyarg(t_printf *t, t_buff *b, const char *arg, int len)
 {
 	char	buff[len + 1];
 	int		slen;
-	//int		x;
 
 	slen = (t->flag == 'c') ? 1 : ft_strlen(arg);
-	//x = (t->nb > 0 && t->minus == 0) ? t->nb - slen : 0;
-	//x += (t->space) ? 1 : 0;
 	ft_bzero(&buff, len + 1);
-	//if (t->prec)
-	//	addzeros(&buff, arg
-	//(t->prec > 0 && t->nb > t->prec && ft_charinset(t->flag, "pibdDxXoOuU")) ? addspaces(buff, t->nb - t->prec, 0) : NULL;
 	(t->prec > slen && ft_charinset(t->flag, "pibdDxXoOuU")) ? addzeros(t, buff, t->prec - slen, 0) : NULL;
 	(t->zero == 1 && t->nb - slen > (int)ft_strlen(buff)) ? addzeros(t, buff, t->nb - slen, ft_strlen(buff)) : NULL;
-	//printf("|%s|\n", buff);
 	(t->nb > t->prec && t->minus == 0 && !buff[0]) ? addspaces(t, buff, t->nb - slen, 0) : NULL;
-	
-	//ft_displaynode(t);
 	(t->space && !t->zero && ft_charinset(t->flag, "dD") && arg[0] != '-') ? addspaces(t, buff, 1, 0) : NULL;
 	(t->plus && !t->zero && !t->prec && ft_charinset(t->flag, "dD") && arg[0] != '-') ? ft_memcpy(buff, "+", 1) : NULL;
-	//(t->zero == 1 && arg[0] == '-' && buff[0] == '0') ? s(buff, arg, 0, ft_strspn(buff, "-")) : NULL ;
-	//(t->neg == 1) ? ft_memcpy(buff, "-", 1) : NULL;
-	//printf("%s\n", buff);
 	ft_memcpy(buff + ft_strlen(buff), arg, slen);
-	//printf("%s\n", buff);
-	//(t->prec > 0 && ft_charinset(t->flag, "pibdDxXoOuU")) ? ft_memcpy(buff + t->prec - slen, arg, slen) : NULL;
 	(t->nb > 0 && t->minus) ? addspaces(t, buff, t->nb - slen, ft_strlen(buff)) : NULL;
-	//printf("%s %d\n", buff, slen);
-	bflush(b, buff, len);
-	//printf("%d %d\n", len, b->pos);
-	//printf("%s\n", (char*) &buff);
+	if (t->prec != -1)
+		bflush(b, buff, len);
 }
 
 int			getarglen(t_printf *t, const char *arg)
@@ -116,10 +88,10 @@ int			getarglen(t_printf *t, const char *arg)
 	len += (ft_charinset(t->flag, "xX") && t->hash == 1) ? 2 : 0; 
 	len += (ft_charinset(t->flag, "oO") && t->hash == 1) ? 1 : 0; 
 	len += (t->plus == 1 && arg[0] != '-') ? 1 : 0; 
-	//len += (t->neg == 1) ? 1 : 0; 
 	len += (t->space == 1 && !t->plus && arg[0] != '-' && ft_charinset(t->flag, "d")) ? 1 : 0; 
 	len = (ft_charinset(t->flag, "sS") && t->prec < len && t->prec > 0) ? t->prec : len;
 	len = (ft_charinset(t->flag, "idDoOxX") && t->prec > len) ? t->prec : len;
+	len += (t->neg) ? 1 : 0; 
 	len += (t->nb > len) ? (t->nb - len) : 0;
 	return (len);
 }
@@ -140,14 +112,10 @@ char		*getarg(t_printf *t, va_list args)
 		str = ft_strdup(&(t->flag));
 		str[1] = '\0';
 	}
-	//printf("%s %d\n", str, t->prec);
-	t->neg = (str[0] == '-' && ft_charinset(t->flag, "dD") && t->zero == 1 && t->nb > (int)ft_strlen(str)) ? 1 : 0;
-	//printf("%s\n", str);
+	t->neg = (str[0] == '-' && ft_charinset(t->flag, "dD") && ((t->zero == 1 && t->nb > (int)ft_strlen(str)) || t->prec > 0)) ? 1 : 0;
 	(t->neg) ? ft_memcpy(str, str + 1, ft_strlen(str)) : NULL;
-	//printf("%s\n", str);
 	if (t->prec > 0 && t->prec < (int)ft_strlen(str) && ft_charinset(t->flag, "sS"))
 		str[t->prec] = '\0';
-	//printf("%s %d\n", str, t->prec);
 	return (str);
 }
 
@@ -162,10 +130,6 @@ void		ft_handler(t_buff *b, const char *fmt, va_list args, int idx)
 	if (!(arg = getarg(&t, args)))
 		return ;
 	arglen = getarglen(&t, arg);
-	//printf("%s %d\n", arg, arglen);
-	//printf("%d\n", arglen);
 	copyarg(&t, b, arg, arglen);
-	//printf("%s\n", arg);
-	//printf("%s\n", va_arg(args, char*));
 	free(arg);
 }
