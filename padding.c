ee10b3afa1d5f9ce8695ef3a0b9690e2c38e31e0
Francois-xavier BABIN <fbabin@e1r6p3.42.fr>
2018-02-15 23:48:48 +0100

    norm ok

--- padding.c
+++ padding.c
@@ -6,13 +6,11 @@
 /*   By: misteir <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/25 18:32:33 by misteir           #+#    #+#             */
-/*   Updated: 2018/02/15 23:20:30 by fbabin           ###   ########.fr       */
+/*   Updated: 2018/02/15 23:47:59 by fbabin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
-
 
 void		add_zeros(t_buff *b, t_printf *t, int len)
 {
@@ -28,7 +26,6 @@ void		add_zeros(t_buff *b, t_printf *t, int len)
 		n -= (t->hash && ft_charinset(t->flag, "xXp")) ? 2 : 0;
 	}
 	n -= (t->hash && ft_charinset(t->flag, "oO")) ? 1 : 0;
-	//n -= (len == 0 && (t->flag == 'o'|| t->flag == 'O')) ? 1 : 0;
 	while (n-- > 0)
 		bflush(b, "0", 1);
 }
@@ -42,16 +39,14 @@ void		add_spaces(t_buff *b, t_printf *t, int len)
 		bflush(b, " ", 1);
 		t->nb--;
 	}
-	//ft_displaynode(t);
-	if (t->zero == 1) //|| (t->prec > t->nb && !t->minus))
+	if (t->zero == 1)
 		return ;
-	//if (!ft_charinset(t->flag, "dDoOxXuUsScCip"))
-	//	return ;
 	n = t->nb - len;
-	//printf("%d\n", len);
 	n += (len == 0 && t->hash && !t->zero && (t->flag == 'o')) ? 1 : 0;
-	n -= (ft_charinset(t->flag, "idDoOxXuU") && t->prec > len) ? t->prec - len : 0;
-	n -= (ft_charinset(t->flag, "idoODxXuUp") && (t->neg || t->plus || t->hash)) ? 1 : 0;
+	n -= (ft_charinset(t->flag, "idDoOxXuU")
+		&& t->prec > len) ? t->prec - len : 0;
+	n -= (ft_charinset(t->flag, "idoODxXuUp")
+		&& (t->neg || t->plus || t->hash)) ? 1 : 0;
 	n -= (t->hash && ft_charinset(t->flag, "pxX")) ? 1 : 0;
 	while (n-- > 0)
 		bflush(b, " ", 1);
@@ -61,8 +56,6 @@ void		ft_padding_b(t_buff *b, t_printf *t, int len)
 {
 	if (!t->minus)
 		add_spaces(b, t, len);
-	//if (!ft_charinset(t->flag, "sS"))
-	//{
 	if (t->hash && ft_charinset(t->flag, "oOxXp"))
 	{
 		bflush(b, "0", 1);
@@ -84,68 +77,7 @@ void		ft_padding_a(t_buff *b, t_printf *t, int len)
 		add_spaces(b, t, len);
 }
 
-/*void	ft_handle_other(t_buff *b, t_printf *t, char flag)
-{
-	ft_padding_b(b, t, 1);
-	bflush(b, &flag, 1);
-	ft_padding_a(b, t, 1);
-}
-
-void	ft_handle_float(t_buff *b, t_printf *t, va_list args)
-{
-	double		nb;
-	char		*str;
-	int			len;
-
-	t->mod1 = (t->flag == 'F') ? 'l' : t->mod1;
-	nb = va_arg(args, double);
-	//if (t->prec == -1)
-	//	return ;
-	if (t->prec == -1)
-	{
-		if (!(str = ft_strnew(0)))
-			return ;
-	}
-	else if (t->prec == 0)
-		str = ft_ftoa(nb, 6);
-	else
-		str = ft_ftoa(nb, t->prec);
-	len = ft_strlen(str);
-	len = (t->prec == -1) ? 0 : len;
-	ft_padding_b(b, t, len);
-	bflush(b, str, len);
-	ft_padding_a(b, t, len);
-	free (str);
-}*/
-
-/*void	ft_handle_colors(t_buff *b, t_printf *t, va_list args)
-{
-	int		nb;
-
-	nb = 
-}*/
-
-/*void	ft_handle_n(t_buff *b, t_printf *t, va_list args)
-{
-	int		*tmp;
-
-	if (!(tmp = va_arg(args, void*)))
-		return ;
-	if (t->mod1 == 'j')
-		*((intmax_t*)tmp) = ((intmax_t)b->len);
-	else if (t->mod1 == 'z')
-		*((size_t*)tmp) = ((size_t)b->len);
-	else if (!(t->mod2) && (t->mod1 == 'h'))
-		*((short*)tmp) = ((short)b->len);
-	else if (t->mod2 == 'h')
-		*((signed char*)tmp) = ((signed char)b->len);
-	else if (!(t->mod2) && (t->mod1 == 'l'))
-		*((long*)tmp) = ((long)b->len);
-	else if (t->mod2 == 'l')
-		*((long long*)tmp) = ((long long)b->len);
-}*/
-
-void	ft_handler(t_buff *b, t_printf *t, va_list args)
+void		ft_handler(t_buff *b, t_printf *t, va_list args)
 {
 	if (ft_charinset(t->flag, "sS"))
 		ft_handle_wstr(b, t, args);
--- xtract.c
+++ xtract.c
@@ -6,27 +6,11 @@
 /*   By: fbabin <fbabin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 20:11:01 by fbabin            #+#    #+#             */
-/*   Updated: 2018/02/15 17:02:43 by fbabin           ###   ########.fr       */
+/*   Updated: 2018/02/15 23:44:28 by fbabin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-/*static int		ft_xtract_number(const char *fmt)
-{
-	int		sign;
-	int		a;
-
-	a = 0;
-	while (ft_isspace(*fmt) || *fmt == '+' || *fmt == '-' || *fmt == '#'
-			|| *fmt == '0')
-		fmt++;
-	sign = (*fmt == '-') ? -1 : 1;
-	fmt += (*fmt == '-' || *fmt == '+') ? 1 : 0;
-	while (*fmt && ft_isdigit(*fmt))
-		a = (a * 10) + (*(fmt++) - '0');
-	return (sign * a);
-}*/
 
 static int		ft_xtract_number(t_printf *t, const char *fmt)
 {
@@ -61,34 +45,15 @@ static int		ft_xtract_float(t_printf *t, const char *fmt)
 	return (i);
 }
 
-/*static int		ft_xtract_float(const char *fmt)
-{
-	int		a;
-
-	a = 0;
-	while ((ft_isspace(*fmt) || *fmt == '+' || *fmt == '-' || *fmt == '#'
-				|| ft_isdigit(*fmt)) && *fmt != '.')
-		fmt++;
-	if ((!*fmt || *fmt == '.') && (fmt[1] == '0' || !ft_isdigit(fmt[1])))
-		return (-1);
-	fmt++;
-	fmt += (*fmt == '-' || *fmt == '+') ? 1 : 0;
-	while (*fmt && ft_isdigit(*fmt))
-		a = (a * 10) + (*(fmt++) - '0');
-	return (a);
-}*/
-
-void		handle_wildcards(t_printf *t, va_list args, char before)
+void			handle_wildcards(t_printf *t, va_list args, char before)
 {
 	int		nb;
 
 	nb = va_arg(args, int);
 	if (nb < 0 && before != '.')
 		t->minus = 1;
-	//nb = ft_abs(nb);
 	if (before == '.')
 	{
-		//t->wild = 2;
 		if (nb < 0)
 			t->prec = 0;
 		else if (nb == 0)
@@ -97,108 +62,52 @@ void		handle_wildcards(t_printf *t, va_list args, char before)
 			t->prec = ft_abs(nb);
 	}
 	else
-	{
 		t->nb = ft_abs(nb);
-		//t->wild = 1;
-	}
-	//printf("\nbefore : %c nb %d prec %d tnb %d\n", before, nb, t->prec, t->nb);
 }
 
-#include <stdio.h>
+void			ft_xtractor_bis(t_printf *t, const char *fmt, va_list args,
+					int *i)
+{
+	if (fmt[*i] == '.')
+	{
+		*i += ft_xtract_float(t, fmt + *i);
+	}
+	else if (ft_isdigit(fmt[*i]))
+	{
+		if (!(t->zero) && ft_isdigit(fmt[*i]))
+			t->zero = (fmt[*i] > '0') ? 2 : 1;
+		*i += ft_xtract_number(t, fmt + *i);
+	}
+	else
+	{
+		t->plus = (!(t->plus) && fmt[*i] == '+') ? 1 : t->plus;
+		t->minus = (!(t->minus) && fmt[*i] == '-') ? 1 : t->minus;
+		t->hash = (!(t->hash) && fmt[*i] == '#') ? 1 : t->hash;
+		t->space = (!(t->space) && fmt[*i] == ' ') ? 1 : t->space;
+		if (!(t->mod2) && (t->mod1) && ft_charinset(fmt[*i], "hl"))
+			t->mod2 = (fmt[*i] == t->mod1) ? fmt[*i] : t->mod2;
+		if ((!(t->mod1) || fmt[*i] == 'l') && ft_charinset(fmt[*i], "hljz"))
+			t->mod1 = fmt[*i];
+		if (fmt[*i] == '*')
+			handle_wildcards(t, args, (*i == 0) ? 'a' : fmt[*i - 1]);
+		*i = *i + 1;
+	}
+}
 
-int			ft_xtractor(t_printf *t, const char *fmt, va_list args)
+int				ft_xtractor(t_printf *t, const char *fmt, va_list args)
 {
 	int			i;
 
 	i = 0;
 	ft_bzero(t, sizeof(t_printf));
-	//ft_displaynode(t);
-	//write(1, &fmt[i+1], 1);
 	while (fmt[i] && ft_charinset(fmt[i], " *#+-.0123456789hljz"))
-	{
-		if (fmt[i] == '.')
-		{
-			i += ft_xtract_float(t, fmt + i);
-			//i++;
-		}
-		else if (ft_isdigit(fmt[i]))
-		{
-			//if (fmt[i] != '0')
-			//printf("%c\n", fmt[i]);
-			if (!(t->zero) && ft_isdigit(fmt[i]))
-				t->zero = (fmt[i] > '0') ? 2 : 1;
-			i += ft_xtract_number(t, fmt + i);
-			//printf("%d\n", i);
-		}
-		else// if (ft_charinset(fmt[i], " *#+-hljz"))
-		{
-			//if (!(t->zero) && ft_isdigit(fmt[i]))
-			//	t->zero = (fmt[i] > '0') ? 2 : 1;
-			t->plus = (!(t->plus) && fmt[i] == '+') ? 1 : t->plus;
-			t->minus = (!(t->minus) && fmt[i] == '-') ? 1 : t->minus;
-			t->hash = (!(t->hash) && fmt[i] == '#') ? 1 : t->hash;
-			t->space = (!(t->space) && fmt[i] == ' ') ? 1 : t->space;
-			if (!(t->mod2) && (t->mod1) && ft_charinset(fmt[i], "hl"))
-				t->mod2 = (fmt[i] == t->mod1) ? fmt[i] : t->mod2;
-			if ((!(t->mod1) || fmt[i] == 'l') && ft_charinset(fmt[i], "hljz"))
-				t->mod1 = fmt[i];
-			if (fmt[i] == '*')
-				handle_wildcards(t, args, (i == 0) ? 'a' : fmt[i - 1]);
-			i++;
-		}
-		//printf("%d\n", i);
-	}
-	//printf("%c", fmt[i - 1]);
-	//i -= (t->zero == 1) ? 1 : 0;
+		ft_xtractor_bis(t, fmt, args, &i);
 	t->flag = fmt[i];
 	if (ft_charinset(t->flag, "dDioOxX") && t->prec == -1)
 		t->zero = 0;
 	if (t->mod1 != t->mod2)
 		t->mod2 = 0;
-	t->zero = (ft_charinset(t->flag, "idDuUoOxX") && (t->minus || t->prec > 0)) ? 0 : t->zero;
-	//if (t->wild == 0 || t->nb == 0)
-	//	t->nb = ft_xtract_number(fmt);
-	//t->nb = (t->prec > t->nb) ? 0 : t->nb;
+	t->zero = (ft_charinset(t->flag, "idDuUoOxX")
+			&& (t->minus || t->prec > 0)) ? 0 : t->zero;
 	return (i);
 }
-
-/*int			ft_xtractor(t_printf *t, const char *fmt, va_list args)
-{
-	int			i;
-
-	i = -1;
-	ft_bzero(t, sizeof(t_printf));
-	//ft_displaynode(t);
-	while (fmt[++i] && ft_charinset(fmt[i], " *#+-.0123456789hljz"))
-	{
-
-		if (!(t->zero) && ft_isdigit(fmt[i]))
-			t->zero = (fmt[i] > '0') ? 2 : 1;
-		t->plus = (!(t->plus) && fmt[i] == '+') ? 1 : t->plus;
-		t->minus = (!(t->minus) && fmt[i] == '-') ? 1 : t->minus;
-		t->hash = (!(t->hash) && fmt[i] == '#') ? 1 : t->hash;
-		t->space = (!(t->space) && fmt[i] == ' ') ? 1 : t->space;
-		if (!(t->mod2) && (t->mod1) && ft_charinset(fmt[i], "hl"))
-			t->mod2 = (fmt[i] == t->mod1) ? fmt[i] : t->mod2;
-		if ((!(t->mod1) || fmt[i] == 'l') && ft_charinset(fmt[i], "hljz"))
-			t->mod1 = fmt[i];
-		if (fmt[i] == '*')
-			handle_wildcards(t, args, (i == 0) ? 'a' : fmt[i - 1]);
-		
-		if (t->wild == 0)
-		{
-			t->prec = ft_xtract_float(fmt );
-		}
-
-	}
-	t->flag = fmt[i];
-	if (ft_charinset(t->flag, "dDioOxX") && t->prec == -1)
-		t->zero = 0;
-	if (t->mod1 != t->mod2)
-		t->mod2 = 0;
-	t->zero = (ft_charinset(t->flag, "idDuUoOxX") && (t->minus || t->prec > 0)) ? 0 : t->zero;
-	if (t->wild == 0 || t->nb == 0)
-		t->nb = ft_xtract_number(fmt);
-	//t->nb = (t->prec > t->nb) ? 0 : t->nb;
-	return (i);
-}*/
